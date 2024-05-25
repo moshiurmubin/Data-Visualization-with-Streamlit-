216b242607c75fa29006226a01af0617ec5a8bc7
moshiurmubin <moshiurmubin096@gmail.com>
2024-05-25 21:24:36 +0600

    initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set the page config
+st.set_page_config(page_title='Data Visualizer',
+                   layout='centered',
+                   page_icon='📊')
